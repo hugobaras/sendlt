ac74548e3dbf00744ab194498b26dc1cd36a663e
hugobaras <98149044+hugobaras@users.noreply.github.com>
2026-06-08 09:18:44 +0200

    chore: bootstrap Spring Boot 4 project
    Java 21, Web MVC, JPA, Validation, Flyway, Lombok. App class only for now.

---
+++ src/main/java/com/sendlt/SendltApplication.java
@@ -0,0 +1,12 @@
+package com.sendlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendltApplication.class, args);
+    }
+}
